65e7c092deb6981bb0c3c36aa34197a503e4b315
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-27 21:29:21 -0400

    Initial version tested and working. Need commenting.

--- src/java_prg_04_06_file_letter_counter/JAVA_PRG_04_06_File_Letter_Counter.java
+++ src/java_prg_04_06_file_letter_counter/JAVA_PRG_04_06_File_Letter_Counter.java
@@ -24,58 +24,78 @@ public class JAVA_PRG_04_06_File_Letter_Counter {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        char chrTemp = 'a'; 
-        char chrTemp2 = 'a';
+        char chrTemp; 
+        char chrTemp2;
         
         String strUserInput;
         String strTemp;
         
-        int intUserInput = 0;
+        // Create variable to hold user input on how many
+        // letters to generate
+        // Create variable and initialize to hold matches
+        // between random letters and user input
+        int intUserInput;
         int intCounter = 0;
         
+        // Create a new random letter generator class
         RandomLetterGenerator chrGenerator = new RandomLetterGenerator();
         
+        // Get user input on how many letters to generate in file
         strUserInput = JOptionPane.showInputDialog("Please enter how many " +
                 "letters\nyou would like to generate.");
         intUserInput = Integer.parseInt(strUserInput);
         
         PrintWriter outputFile = new PrintWriter("/Users/bluebackdev/NetBeansProjects/Github/Book/Ch04 - Loops and Files/JAVA_PRG_04_06_File_Letter_Counter/textfile.txt");
         
+        // Keep generating a letter and incrementing through the loop
+        // to fill the file until user input limit has been reached
         for(int i = 0 ; i < intUserInput ; i++)
         {
             chrTemp = chrGenerator.getRandomCharacter();
             outputFile.println(chrTemp);
         }
         
+        // Remember to close the file
         outputFile.close();
         
+        // Prompt user input on letter to search file for
         strUserInput = JOptionPane.showInputDialog("Please enter a letter " +
                 "to search for.");
         
+        // Make sure the comparison is in the correct case
+        // (using uppercase ASCII values in random generator)
         strUserInput.toUpperCase();
         
+        // Get user input (first character of input just in case)
         chrTemp = strUserInput.charAt(0);
         
+        // Open the file
         File file = new File("/Users/bluebackdev/NetBeansProjects/Github/Book/Ch04 - Loops and Files/JAVA_PRG_04_06_File_Letter_Counter/textfile.txt");
         Scanner inputFile = new Scanner(file);
         
+        // Compare each line to user input
         for(int i = 0 ; i < intUserInput ; i++)
         {
             strTemp = inputFile.nextLine();
             
             chrTemp2 = strTemp.charAt(0);
             
+            // If the current letter matches the user input
+            // then increment the counter variable
             if(chrTemp == chrTemp2)
             {
                 intCounter++;
             }
         }
         
+        // Remember to close the file
         inputFile.close();
         
+        // Display results to the user
         JOptionPane.showMessageDialog(null, "Your letter appears " + intCounter
             + " times in the generated file.");
         
+        // Exit for JOptionPane
         System.exit(0);
     }
     
