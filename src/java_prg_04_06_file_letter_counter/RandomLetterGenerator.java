35594ab93d6db7f7c6916b30a7041675290e1046
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-27 21:32:20 -0400

    Initial version tested and working. Need commenting.

--- src/java_prg_04_06_file_letter_counter/RandomLetterGenerator.java
+++ src/java_prg_04_06_file_letter_counter/RandomLetterGenerator.java
@@ -16,21 +16,33 @@ public class RandomLetterGenerator {
     
     public char getRandomCharacter()
     {
-        final int INT_MIN = 0;
+        // Create constant for limiting random
+        // int generator to 0-25 (total number
+        // of letters in the alphabet)
         final int INT_MAX = 25;
 
+        // Declare temp holder to hold each randomly
+        // generated number before it's converted to a char
         int intTempLetterHolder;
 
+        // Temporary character variable to return
+        // with random letter for user in main
         char chrTempToWrite;
 
+        // Create random generator
         Random randomLetters = new Random();
 
+        // Generate number and offset by 65
+        // to get to letters on the ASCII chart
         intTempLetterHolder = randomLetters.nextInt(INT_MAX) + 65;
 
+        // Convert int value to character
         chrTempToWrite = (char) intTempLetterHolder;
 
+        // Debug
 //        JOptionPane.showMessageDialog(null, chrTempToWrite);
 
+        // Return generated letter
         return chrTempToWrite;
     }
 }
